5483a1e19f547392f17b19ed1380bc526fe068ba
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2025-05-08 23:49:11 -0300

    Add Menu Controller

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,54 @@
+class MenusController < ApplicationController
+  before_action :set_menu, only: %i[ show update destroy ]
+
+  # GET /menus or /menus.json
+  def index
+    @menus = Menu.all
+    render json: @menus
+  end
+
+  # GET /menus/1 or /menus/1.json
+  def show
+    render json: @menu
+  end
+
+  # POST /menus or /menus.json
+  def create
+    @menu = Menu.new(menu_params)
+
+    if @menu.save
+      render json: @menu, status: :created
+    else
+      render json: { errors: @menu.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /menus/1 or /menus/1.json
+  def update
+    if @menu.update(menu_params)
+      render json: @menu, status: :ok
+    else
+      render json: { errors: @menu.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /menus/1 or /menus/1.json
+  def destroy
+    @menu.destroy!
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_menu
+      @menu = Menu.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def menu_params
+      params.expect(menu: [ :name ])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :menus, only: [ :index, :show, :create, :update, :destroy ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +12,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Route to catch any other request and redirect to React
+  get "*path", to: "menus#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
