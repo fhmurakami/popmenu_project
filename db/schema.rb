a16c9175316790cd941c90fdd6d0801b75900ebf
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2025-05-09 16:30:27 -0300

    Add MenuItem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_08_215541) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_09_190932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.decimal "price", precision: 5, scale: 2, null: false
+    t.string "category"
+    t.string "dietary_restrictions"
+    t.text "ingredients"
+    t.bigint "menu_id", null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+    t.index ["name"], name: "index_menu_items_on_name", unique: true
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "menu_items", "menus"
 end
