dc38b01090ece084636679f7fce9b740367c15d0
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2025-05-12 15:21:04 -0300

    Move Bootstrap to a link tag for better performance and caching

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,26 +4,24 @@ import Navbar from "./Navbar"
 import MenuList from "./MenuList"
 // import MenuDetail from "./MenuDetail"
 import MenuForm from "./MenuForm"
-import "bootstrap/dist/css/bootstrap.min.css"
 import "../App.css"
 
 const App = () => {
-  console.log("App component rendered")
-  return (
-    <Router>
-      <div className="App min-h-screen bg-gray-100">
-        <Navbar />
-        <div className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<MenuList />} />
-            <Route path="/menus" element={<MenuList />} />
-            <Route path="/menus/new" element={<MenuForm />} />
-            <Route path="/menus/:id/edit" element={<MenuForm />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<div className="App min-h-screen bg-gray-100">
+				<Navbar />
+				<div className="container mx-auto px-4 py-8">
+					<Routes>
+						<Route path="/" element={<MenuList />} />
+						<Route path="/menus" element={<MenuList />} />
+						<Route path="/menus/new" element={<MenuForm />} />
+						<Route path="/menus/:id/edit" element={<MenuForm />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	)
 }
 
 export default App
