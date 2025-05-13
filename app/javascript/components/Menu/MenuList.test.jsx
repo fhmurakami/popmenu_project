6df4c4e6dc4b34e3bdc18b7cf920889536275e71
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2025-05-13 01:46:50 -0300

    Remove unnecessary comment

--- app/javascript/components/Menu/MenuList.test.jsx
+++ app/javascript/components/Menu/MenuList.test.jsx
@@ -1,4 +1,3 @@
-// import React from "react"
 import { render, screen, fireEvent, waitFor } from "@testing-library/react"
 import { BrowserRouter as Router } from "react-router-dom"
 import "@testing-library/jest-dom"
