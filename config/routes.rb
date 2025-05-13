06eb4530e80ee23f1970e7ede2f8720e91f587ef
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2025-05-13 01:59:18 -0300

    Minor adjustments on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,15 @@ Rails.application.routes.draw do
       end
     end
   end
-  get "home/index"
-  root "home#index"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Catch-all route for React Router
-  get "*path", to: "menus#index", constraints: ->(request) do
+  get "*path", to: "home#index", via: :all, constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
+
+  root "home#index"
 end
