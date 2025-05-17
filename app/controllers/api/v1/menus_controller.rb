022a0a4399f56afb2d15cc4bfe2a547841d448b8
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2025-05-17 16:15:43 -0300

    feat(api): add Menu blueprint and integrate with MenusController
    - Added MenuBlueprint to handle JSON serialization of Menu objects
    - Updated MenusController to use MenuBlueprint in render responses

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -1,20 +1,17 @@
 class Api::V1::MenusController < ApplicationController
   before_action :set_menu, only: %i[ show update destroy ]
+  before_action :set_restaurant
 
   # GET /menus or /menus.json
   def index
-    @menus = Menu.all
-    render json: @menus, include: :menu_items
+    @menus = @restaurant.menus
+    render json: MenuBlueprint.render(@menus)
   end
 
   # GET /menus/1 or /menus/1.json
   def show
-    render json: @menu, include: :menu_items
-  end
-
-  def new
-    @menu = Menu.new
-    render json: @menu
+    @menu = @restaurant.menus.find(params[:id])
+    render json: MenuBlueprint.render(@menu)
   end
 
   # POST /menus or /menus.json
@@ -22,7 +19,7 @@ class Api::V1::MenusController < ApplicationController
     @menu = Menu.new(menu_params)
 
     if @menu.save
-      render json: @menu, include: :menu_items, status: :created
+      render json: MenuBlueprint.render(@menu), status: :created
     else
       render json: { errors: @menu.errors }, status: :unprocessable_entity
     end
@@ -31,7 +28,7 @@ class Api::V1::MenusController < ApplicationController
   # PATCH/PUT /menus/1 or /menus/1.json
   def update
     if @menu.update(menu_params)
-      render json: @menu, include: :menu_items, status: :ok
+      render json: MenuBlueprint.render(@menu), status: :ok
     else
       render json: { errors: @menu.errors }, status: :unprocessable_entity
     end
@@ -50,6 +47,10 @@ class Api::V1::MenusController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_menu
       @menu = Menu.find(params.expect(:id))
+    end
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params.expect(:restaurant_id))
     end
 
     # Only allow a list of trusted parameters through.
